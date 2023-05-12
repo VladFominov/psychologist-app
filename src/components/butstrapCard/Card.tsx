4421177f293ea13e3b1bedb6cbcc9e84b1d148bb
Vlad <Vlad.fominov11@gmail.com>
2023-05-12 13:03:27 +0300

    Img responsive

--- src/components/butstrapCard/Card.tsx
+++ src/components/butstrapCard/Card.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import Image from "next/image";
+import Image from 'next/image';
 
 import cardData from "../cardData/CardData";
 import Modal from "../butstrapModal/Modal";
@@ -13,14 +13,17 @@ const FetchCard: FC = () => {
   return (
     <>
       {cardData.map(({ photo, title, description, index }) => (
-        <div className="card shadow-sm p-4 mb-4 mt-3 bg-white" key={index} style={{ }}>
+        <div className="card shadow-sm p-4 mb-4 mt-3 bg-white" key={index}>
+          <div className={styles.cardImgWrap}>
           <Image
             src={photo}
-            className="card-img-top"
-            width={500}
-            height={200}
+            className="card-img-top "
+            style={{objectFit: "cover"}}
+            fill={true} 
             alt="Слово Залежність"
           />
+          </div>
+         
           <div className="card-body p-0 mt-3">
             <h5 className="card-title"><span className={styles.cardTitleStyles}>{title}</span></h5>
             <ul className="card-text">
