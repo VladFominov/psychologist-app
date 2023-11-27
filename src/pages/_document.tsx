e9611cffb2102084797dfc6b09c508469a418947
unknown <vlad.fominov11@gmail.com>
2023-11-27 17:35:45 +0300

    add meta for google console

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
-
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
- 
   return (
     <Html lang="uk">
       <Head>
-          <link rel="stylesheet"
-           href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" 
-           integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" 
-           crossOrigin="anonymous">
-           </link>
-          
+        <meta
+          name="google-site-verification"
+          content="t6VSTP_V7JgmvsEsWbNBAihdLn6HaHIw3YpMtOTSeaU"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+          integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+          crossOrigin="anonymous"
+        ></link>
       </Head>
-      <body >
+      <body>
         <Main />
-        <NextScript />      
+        <NextScript />
       </body>
     </Html>
-  )
+  );
 }
