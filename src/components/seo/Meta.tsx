210e50da5242f9790b5444443633b2f1b369e953
unknown <vlad.fominov11@gmail.com>
2024-07-07 18:05:22 +0300

    added some CEO

--- src/components/seo/Meta.tsx
+++ src/components/seo/Meta.tsx
@@ -2,7 +2,7 @@ import React, { FC, PropsWithChildren } from "react";
 import { IMeta } from "./meta.interface";
 import Head from "next/head";
 
-const getTitle = (title: string) => `${title} | Psychologist`;
+const getTitle = (title: string) => `${title} | Олексій Більський - Психолог`;
 
 const Meta: FC<PropsWithChildren<IMeta>> = ({
   title,
@@ -14,6 +14,10 @@ const Meta: FC<PropsWithChildren<IMeta>> = ({
       <Head>
         <title>{getTitle(title)}</title>
         <meta name="description" content={description} />
+        <meta
+          name="keywords"
+          content="психолог, Олексій Більський, психологічна допомога, ПТСР, консультація депресії, лікування панічних атак, консультація при алкоголізмі, Україна, Київ, онлайн консультації, військовий психолог, консультації по всій Україні онлайн, психотерапія, аддиктологія, психологічні послуги, психологічна підтримка, терапія, психолог онлайн, лікування стресу, лікування тривожності, допомога при депресії, психологічна консультація, психотерапевт"
+        />
         <meta name="og:title" content={getTitle(title)} />
         <meta name="og:description" content={description} />
       </Head>
